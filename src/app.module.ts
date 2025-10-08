fc3c1828c24a25ea7330e358cfeaea2a1e55a37c
Pablo Andres Chacon <pabloandres120297@gmail.com>
2025-10-07 21:06:59 -0500

    conect to mongodb and pokemon schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
 import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    MongooseModule.forRoot(
+      'mongodb+srv://root:M0nG0D3root@cluster0.c2xwqwo.mongodb.net/',
+    ),
+    PokemonModule,
   ],
 })
 export class AppModule {}
